95464735016f94a5c5b9110183dd74bfb7e7784a
Juliya Koltsova <juliyaasking009@gmail.com>
2021-04-01 21:15:13 +0200

    Included script into HTML-file, created variables, a modal window and console output

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
+// variables
 
+let money = '';
+let income = '';
+let addExpenses = '';
+let deposit = '';
+let mission = '';
+let period = '';
+
+// modal window
+
+alert("Hello World!");
+
+// console output
+
+console.log("Hi, there!");
 
