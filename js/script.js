8fead5c01fb6f59b9df847f8b24226d6748033a5
Juliya Koltsova <juliyaasking009@gmail.com>
2021-04-03 01:49:47 +0200

    lesson02 done

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-// variables
+// VARIABLES
 
-let money = '';
-let income = '';
-let addExpenses = '';
-let deposit = '';
-let mission = '';
-let period = '';
+let money = 20000;
+let income = 'Муж даёт';
+let addExpenses = 'Коммуналка, Еда, Бензин, Интернет, Бытовые мелочи, Развлечения';
+let deposit = true;
+let mission = 30000;
+let period = 6;
 
-// modal window
+let budgetDay = (money / 30);
 
-alert("Hello World!");
+// MODAL WINDOW
 
-// console output
+//alert("Hello World!");
 
-console.log("Hi, there!");
+// CONSOLE OUTPUT
+
+//console.log("Hi, there!");
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей/долларов/гривен/юани');
+
+console.log();
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+console.log(budgetDay);
 
